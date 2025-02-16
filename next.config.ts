efdcf228e9a6c4eeab2c32fd60b694b918ae1006
rakeshvs1202 <rakeshgem79@gmail.com>
2025-02-16 18:13:37 +0530

    changed folder structure

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true, // Ensures URLs end with a slash
-  output: "export", // Required if using `next export` for static deployment
 };
 
 export default nextConfig;
